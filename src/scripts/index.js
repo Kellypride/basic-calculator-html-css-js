90174973a8576b5524df955586205b9d720a778b
kellypride <ngehkellypride181@gmai.com>
2024-01-17 15:46:01 +0100

    created function to calculate trigonometry

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,82 @@
-function main() {
-    return "hello world";
+// function to display values on the screen
+function display(value) {
+    let result = document.getElementById("result");
+    result.value += value;
+ }
+
+//  function to clear result on the screen
+ function clearScreen() {
+    let result = document.getElementById("result");
+    result.value = '';
+ }
+
+function calculate() {
+    let result = document.getElementById("result");
+    try {
+        result.value = eval(result.value);
+    }catch (error){
+        result.value = 'Error';
+    }
 }
+
+// function to calculate log 
+function calculateLog() {
+     let result = document.getElementById("result");
+     try {
+        result.value = Math.log10(eval(result.value));
+     }catch (error){
+        result.value = 'Error';
+    }
+}
+
+// function to calculate square root 
+function calculatesqrt() {
+    let result = document.getElementById("result");
+    try {
+        result.value = Math.sqrt(eval(result.value));
+    }catch (error){
+        result.value = 'error';
+    }
+}
+// trigonometry functions
+// function to calculate sin 
+function calculateSin(sin) {
+    let result = document.getElementById("result");
+    try {
+        result.value = Math.sin(eval(result.value));
+    } catch (error) {
+        result.value = 'Error';
+    }
+}
+
+// function to calculate cos 
+function calculateCos() {
+    let result = document.getElementById("result");
+    try {
+        result.value = Math.cos(eval(result.value));
+    } catch (error) {
+        result.value = 'Error';
+    }
+}
+
+// function to calculate Tan 
+function calculateTan() {
+    let result = document.getElementById("result");
+    try {
+        result.value = Math.tan(eval(result.value));
+    } catch (error) {
+        result.value = 'Error';
+    }
+}
+
+//function to calculate power of a number
+function calculatepow() {
+    let result = document.getElementById("result");
+    try {
+        result.value = Math.pow(2,(eval(result.value)));
+    } catch (error) {
+        result.value = 'Error';
+    }
+}
+
+
