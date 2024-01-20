eedb3231f959f866c1a8393f018dcb4f7db218d9
kellypride <ngehkellypride181@gmai.com>
2024-01-20 23:26:59 +0100

    Mmath functions of scientifi calculated

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,82 +1,136 @@
-// function to display values on the screen
-function display(value) {
-    let result = document.getElementById("result");
-    result.value += value;
- }
+// function to display numbers
+// get all buttons with id and classname
+const numberButtons = document.querySelectorAll('#numbers.button, #operators.button, #bracket.button');
+// iterate over each button and diplsy value 
+numberButtons.forEach((button) => {
+    button.addEventListener('click', function() {
+        const display = document.getElementById('result');
+        result.value += button.innerText;
+    });
+});
 
-//  function to clear result on the screen
- function clearScreen() {
-    let result = document.getElementById("result");
-    result.value = '';
- }
+//function evaluator takes 3 paraments the result, function pass and the values passed
+function evaluator(output, func, ...params) {
+    const ans = func(...params)
+    let res = document.getElementById(output)
+    res.value = ans
+}
 
-function calculate() {
-    let result = document.getElementById("result");
+//function to calculate pass an expression
+function calculate(expression) {
     try {
-        result.value = eval(result.value);
+        return eval(expression);
     }catch (error){
-        result.value = 'Error';
+        return'Error';
     }
 }
 
-// function to calculate log 
-function calculateLog() {
-     let result = document.getElementById("result");
-     try {
-        result.value = Math.log10(eval(result.value));
-     }catch (error){
-        result.value = 'Error';
+//function delete results one step to the right
+function deleteResult(num) {
+    if (num.length > 0) {
+        return num.slice(0, -1);
     }
+}
+
+//  function to clear result on the screen
+ function clearScreen(num) {
+    return '';
+ }
+
+// function to calculate log 
+function calculateLog(num) {
+    return Math.log10(num);
 }
 
 // function to calculate square root 
-function calculatesqrt() {
-    let result = document.getElementById("result");
-    try {
-        result.value = Math.sqrt(eval(result.value));
-    }catch (error){
-        result.value = 'error';
-    }
+function calculatesqrt(num) {
+    return Math.sqrt(num);
 }
+
 // trigonometry functions
 // function to calculate sin 
-function calculateSin(sin) {
-    let result = document.getElementById("result");
-    try {
-        result.value = Math.sin(eval(result.value));
-    } catch (error) {
-        result.value = 'Error';
-    }
+function calculateSin(num) {
+    return  Math.sin(num);
 }
 
 // function to calculate cos 
-function calculateCos() {
-    let result = document.getElementById("result");
-    try {
-        result.value = Math.cos(eval(result.value));
-    } catch (error) {
-        result.value = 'Error';
-    }
+function calculateCos(num) {
+    return Math.cos(num);
 }
 
 // function to calculate Tan 
-function calculateTan() {
-    let result = document.getElementById("result");
-    try {
-        result.value = Math.tan(eval(result.value));
-    } catch (error) {
-        result.value = 'Error';
-    }
+function calculateTan(num) {
+    return Math.tan(num);
+}
+
+// function to calculate log 
+function calculateLn(num) {
+    return Math.log(num)    
 }
 
 //function to calculate power of a number
-function calculatepow() {
-    let result = document.getElementById("result");
-    try {
-        result.value = Math.pow(2,(eval(result.value)));
-    } catch (error) {
-        result.value = 'Error';
+function calculatepow(num) {
+    return Math.pow((num),2);
+}
+
+//function to calculate pi
+ function calculatePi(num) {
+    return (num) * Math.PI;
+ }
+
+ //function to calculate exponenent of a number
+function calculateExponent(num) {
+    return Math.exp(num);
+}
+
+//function to calculate radians
+function calculateRadian(num) {
+     return (num)*Math.PI / 180;
+}
+
+//function to calculate degree
+function calculateDegree(num) {
+   return (num)*180 / Math.PI;
+}
+
+//function to calculate fatorial
+function calculateFactorial(num) {
+    let number = 1;
+    if (num === 0) {
+        num = '1';
+    } else if (num < 0) {
+        num = NaN;
+    } else {
+        let number = 1;
+        for (let i = num; i > 0; i--) {
+            number *= i;
+        }
+        return number;
     }
 }
 
+const powButtons = document.querySelectorAll('#powerNumber.button');
+// iterate over each button and diplsy value 
+powButtons.forEach((button) => {
+    button.addEventListener('click', function() {
+        const display = document.getElementById('result');
+        result.value += button.innerText="^";
+    });
+});
 
+function baseToThePower(base, power){
+    let prod = 1;
+    for(i=0; i<power; i++){
+        prod *= base; 
+    }   
+    return prod;
+}
+
+console.log(baseToThePower(2, 5));
+function calculateIndice() {
+    let result = document.getElementById("result");
+    let parts=result.split("^");
+    let base = eval(parts[0])
+    let power = eval(parts[1]);
+    result.value = baseToThePower(base, power);
+}
